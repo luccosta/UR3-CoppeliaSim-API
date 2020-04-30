9697ebbbd6ba8a515d7965745b06c064c15e2920
Lucas Fernando <lucas.costa@ee.ufcg.edu.br>
2020-04-30 17:36:55 -0300

    Organize directories and modify demo

--- demo.py
+++ demo.py
@@ -1,18 +1,27 @@
+import sys
+sys.path.insert(1, 'src')
+
 import sim
-import time
 from ur3_api import UR3
+
+import time
+import random
 
 ur3_sim = UR3()
 
 ur3_sim.run_coppelia()
 
-joint_values = [1, 0.2, 0.4, -0.8, 0.5, -1] # random position
 initial_state = [0, 0, 0, 0, 0, 0]
 
-ur3_sim.joint_values(joint_values)
+for i in range (5):
+    random.seed(i)
+    joint_values = [random.random(), random.random(), random.random(), 
+                    random.random(), random.random(), random.random()]
 
-time.sleep(2)
+    ur3_sim.joint_values(joint_values)
+    time.sleep(1)
 
-ur3_sim.joint_values(initial_state)
+    ur3_sim.joint_values(initial_state)
+    time.sleep(1)
 
 ur3_sim.stop_simulation()
